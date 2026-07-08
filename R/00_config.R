e04e1ca09917432c3e4f8ba99116f3ea85a068f0
Ines-gpm <inesgpmarco@gmail.com>
2026-07-08 10:32:31 +0200

    Crear estructura inicial de la configuracion

---
+++ R/00_config.R
@@ -0,0 +1,34 @@
+
+# 00_config.R: Carga de librerías
+
+# ---------------------------------------------------------------------------
+
+# 1. Creación de entidades (ETL)
+
+## Lectura de archivos CSV
+library(readr)  
+
+## Manipulación de datos y joins
+library(tidyverse)
+
+## Limpieza de nombres de variables
+library(janitor)
+
+## Manejo de fechas
+library(lubridate)  
+
+# ---------------------------------------------------------------------------
+
+# 2. Análisis descriptivo
+
+
+# ---------------------------------------------------------------------------
+
+# 3. Visualización de datos
+
+
+# ---------------------------------------------------------------------------
+
+# 4. Modelos de regresión
+
+
