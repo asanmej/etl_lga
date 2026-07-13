cb93b211b1b4f09cee8cb12837396faf10b3c389
Ines-gpm <inesgpmarco@gmail.com>
2026-07-13 17:34:09 +0200

    SITUACION_ADMIN_HIJO completa formato fechas
    - Corregido el formato de lectura de fechas
    - Reordenada la creación de la clave primaria y el renombrado de atributos
    - Añadidos comentarios explicativos para mejorar la documentación del proceso ETL
    - Adaptado el formato de exportación de fechas a YYYYMMDD
    - Añadida la exportación de la entidad transformada a CSV

--- R/06_build_situacion_admin_hijo.R
+++ R/06_build_situacion_admin_hijo.R
@@ -1,3 +1,4 @@
+
 source("00_config.R")
 
 # Importar los datos principales de la entidad SITUACION_ADMIN_HIJO
@@ -9,21 +10,24 @@ hijo_demograficos <- hijo_demograficos %>%
   clean_names() %>% 
   distinct() %>% 
   mutate(
-    altabdu_dt = as.Date(altabdu_dt, format = "%d-%m-%Y"),
-    bajabdu_dt = as.Date(bajabdu_dt, format = "%d-%m-%Y")
+    altabdu_dt = as.Date(altabdu_dt, format = "%d/%m/%Y"),
+    bajabdu_dt = as.Date(bajabdu_dt, format = "%d/%m/%Y")
   )
 
-# Crear la clave primaria de la entidad
-situacion_admin_hijo <- hijo_demograficos %>%
+# Adaptar los nombres de los atributos a la nomenclatura definida en el modelo E/R
+situacion_admin_hijo <- hijo_demograficos %>% 
+  rename(
+    id_hijo = patient_id,
+    indice_privacion = ind_privacion
+  )
+
+# Generar una clave primaria artificial para identificar de forma única
+# cada registro administrativo
+situacion_admin_hijo <- situacion_admin_hijo %>%
   mutate(
     id_admin_hijo = row_number()
-    ) %>%
-    relocate(id_admin_hijo)
-
-# Adaptar los nombres de las variables al modelo E/R
-situacion_admin_hijo <- situacion_admin_hijo %>% 
-  rename(id_hijo = patient_id,
-         indice_privacion = ind_privacion)
+  ) %>%
+  relocate(id_admin_hijo)
 
 # Conservar únicamente las variables definidas para la entidad
 # SITUACION_ADMIN_HIJO
@@ -38,3 +42,12 @@ situacion_admin_hijo <- situacion_admin_hijo %>%
     bajabdu_dt,
     motivo_baja
   )
+
+# Adaptar las fechas al formato YYYYMMDD definido para la exportación
+situacion_admin_hijo <- situacion_admin_hijo %>%
+  mutate(
+    altabdu_dt = format(altabdu_dt, "%Y%m%d"),
+    bajabdu_dt = format(bajabdu_dt, "%Y%m%d")
+  )
+
+write_csv(situacion_admin_hijo, "Y:/PROYECTOS/2024 Salud perinatal (Luis-Aída-Sol)/Desarrollo/Datos_transformados/situacion_admin_hijo.csv")
