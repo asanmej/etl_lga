8b667a8a85641c07236ef795729346f6c08debae
Ines-gpm <inesgpmarco@gmail.com>
2026-07-14 14:26:59 +0200

    Eliminación de redundancias
    Gracias al script 02_reconstruccion_parto_embarazo.R se pueden eliminar tanto de HIJO como de EMBARAZO aquellas partes comunes de código y creación de tablas auxiliares de embarazo y parto

--- R/04_build_hijo.R
+++ R/04_build_hijo.R
@@ -1,22 +1,8 @@
-
-source("00_config.R")
-
-
 # Importar las fuentes de datos necesarias para construir la entidad HIJO
-hijo_neosoft <- read_delim("Y:/PROYECTOS/2024 Salud perinatal (Luis-Aída-Sol)/Desarrollo/Datos/csv_20240508/hijo_neosoft.csv", 
-                           delim = "|", escape_double = FALSE, trim_ws = TRUE)
+# hijo_neosoft, hijo_demograficos, madre_cartilla y embarazos_aux
+# se generan en 03_reconstruccion_embarazos.R
 
-hijo_demograficos <- read_delim("Y:/PROYECTOS/2024 Salud perinatal (Luis-Aída-Sol)/Desarrollo/Datos/csv20260616/hijo_demograficos.csv", 
-                                delim = "|", escape_double = FALSE, trim_ws = TRUE)
-
-# Estandarizar nombres de variables y eliminar posibles registros duplicados
-hijo_neosoft <- hijo_neosoft %>%  
-  clean_names() %>%  
-  distinct()        
-
-hijo_demograficos <- hijo_demograficos %>%
-  clean_names() %>%
-  distinct()
+# Limpieza en 03_reconstruccion_embarazos.R
 
 # Seleccionar los atributos principales que definen la entidad HIJO
 hijo <- hijo_neosoft %>%
@@ -33,7 +19,7 @@ hijo <- hijo_neosoft %>%
 
 # Construir la fecha de nacimiento a partir del año y el mes disponibles.
 # Al desconocerse el día, se asigna el primer día del mes según la convención
-# definida en la documentación del proyecto.
+# definida en la documentación del proyecto
 hijo_demograficos <- hijo_demograficos %>%
   mutate(
     fecha_nacimiento = as.Date(
@@ -48,7 +34,8 @@ hijo_demograficos <- hijo_demograficos %>%
 # Incorporar la fecha de nacimiento a la entidad HIJO
 hijo <- hijo %>%
   left_join(
-    hijo_demograficos,
+    hijo_demograficos %>%
+      select(patient_id, fecha_nacimiento),
     by = "patient_id"
   )
 
@@ -57,9 +44,8 @@ hijo <- hijo %>%
   rename(id_hijo = patient_id, 
          edad_gestacional_nacimiento = edad_gestacional)
 
-# Tabla auxiliar utilizada únicamente durante el proceso ETL para relacionar cada 
-# recién nacido con el embarazo correspondiente. No forma parte del modelo entidad-relación final.
-hijo_aux <- hijo_neosoft %>%
+# Construir una tabla auxiliar con los hijos y la madre correspondiente
+hijos_aux <- hijo_neosoft %>%
   select(
     patient_id,
     mother_patient_id
@@ -74,55 +60,52 @@ hijo_aux <- hijo_neosoft %>%
   ) %>%
   distinct()
 
-
-
-
-# Comprobaciones a ejecutar mañana
-
-## Para ver que no haya duplicados
-hijo_aux %>%
-  count(id_hijo) %>%
-  filter(n > 1)
-
-## Comprobar cuántos hijos tiene cada madre
-hijo_aux %>%
-  count(id_madre)
-
-## Madres con más de un hijo registrado
-hijo_aux %>%
-  count(id_madre) %>%
-  filter(n > 1)
-
-## Mirar algún ejemplo concreto
-hijo_aux %>%
-  filter(id_madre == "XXXX")
-
-# Una vez construida la entidad EMBARAZO, comprobar que la fecha de nacimiento
-# del hijo permite identificar correctamente el embarazo correspondiente.
-# embarazo %>%
-#   filter(id_madre == "XXXX")
-
-## Comprobar que para una misma madre no existan dos hijos nacidos el mismo mes.
-## Porque si aparecen dos eso son gemelos (o trillizos) Y entonces ambos deberán 
-## compartir el mismo id_embarazo
-hijo_aux %>%
-  count(id_madre, fecha_nacimiento) %>%
-  filter(n > 1)
-
-
-
-# join final (primero comprovar todo lo anterior)
-hijo_aux <- hijo_aux %>%
+hijos_aux <- hijos_aux %>%
   left_join(
-    embarazo %>%
-      select(id_embarazo,
-             id_madre,
-             fecha_parto),
-    by = c(
-      "id_madre",
-      "fecha_nacimiento" = "fecha_parto"
-    )
+    embarazos_aux %>%
+      select(
+        id_embarazo,
+        id_madre,
+        fecha_parto
+      ),
+    by = "id_madre"
+  ) %>%
+  mutate(diferencia = abs(as.numeric(fecha_nacimiento - fecha_parto))
+  ) %>%
+  group_by(id_hijo) %>%
+  slice_min(diferencia,
+            n = 1,
+            with_ties =  FALSE
+  ) %>%
+  ungroup()
+
+hijo <- hijo %>%
+  left_join(
+    hijos_aux %>%
+      select(
+        id_hijo,
+        id_embarazo
+      ),
+    by = "id_hijo")
+
+# Formatear las fechas siguiendo el estándar YYYYMMDD  
+hijo <- hijo %>%
+  mutate(
+    fecha_nacimiento = format(fecha_nacimiento, "%Y%m%d")
   )
 
-#View(hijo)
+# Reordenar variables 
+hijo <- hijo %>%
+  select(
+    id_hijo,
+    id_embarazo,
+    peso_nacimiento,
+    talla_nacimiento,
+    perimetro_craneal,
+    edad_gestacional_nacimiento,
+    malformation_cd,
+    muerte_neonatal,
+    fecha_nacimiento
+  )
 
+write_csv(hijo,"Y:/PROYECTOS/2024 Salud perinatal (Luis-Aída-Sol)/Desarrollo/Datos_transformados/hijo.csv")
