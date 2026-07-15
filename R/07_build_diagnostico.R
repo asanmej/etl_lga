60e55dc9c93367be655d6b02ed99504f37ebc114
Ines-gpm <inesgpmarco@gmail.com>
2026-07-15 11:27:07 +0200

    Filtrar madres (solo neosoft y cartilla)

--- R/07_build_diagnostico.R
+++ R/07_build_diagnostico.R
@@ -14,20 +14,48 @@ hijo_diag_omi <- read_delim("Y:/PROYECTOS/2024 Salud perinatal (Luis-Aída-Sol)/
 hijo_neosoft <- read_delim("Y:/PROYECTOS/2024 Salud perinatal (Luis-Aída-Sol)/Desarrollo/Datos/csv_20240508/hijo_neosoft.csv", 
                            delim = "|", escape_double = FALSE, trim_ws = TRUE)
 
-# Limpieza de los datos
+# Creamos una tabla de madres válidas que emplearemos al final para filtrar
+madres_validas <- hijo_neosoft %>%
+  distinct(mother_patient_id) %>%
+  rename(id_madre = mother_patient_id) %>%
+  inner_join(
+    madre_cartilla %>%
+      distinct(patient_id) %>%
+      rename(id_madre = patient_id),
+    by = "id_madre"
+  )
+
+# Limpieza de los datos y filtramos haciendo uso de madres_validas para los diagnosticos
+# y la entidad HIJO para los diagnosticos de los hijos (ejecutada anterior mente en Main.R)
 madre_diag_omi <- madre_diag_omi %>%
   clean_names() %>%
   distinct() %>%
-  filter(patient_id %in% hijo_neosoft$mother_patient_id)
+  rename(id_madre = patient_id) %>%
+  semi_join(
+    madres_validas,
+    by = "id_madre"
+  ) %>%
+  rename(patient_id = id_madre)
 
 madre_diag_cmbd <- madre_diag_cmbd %>%
   clean_names() %>%
   distinct() %>%
-  filter(patient_id %in% hijo_neosoft$mother_patient_id)
+  rename(id_madre = patient_id) %>%
+  semi_join(
+    madres_validas,
+    by = "id_madre"
+  ) %>%
+  rename(patient_id = id_madre)
 
 hijo_diag_omi <- hijo_diag_omi %>%
   clean_names() %>%
   distinct()
+
+hijo_diag_omi <- hijo_diag_omi %>%
+  semi_join(
+    hijo %>% select(id_hijo),
+    by = c("patient_id" = "id_hijo")
+  )
 
 # Identificar el tipo de paciente y el origen de cada diagnóstico
 diag_omi <- madre_diag_omi %>%
@@ -68,6 +96,7 @@ diagnostico <- diagnostico %>%
 # diferenciándose mediante la variable tipo_paciente.
 diagnostico <- diagnostico %>%
   rename(id_paciente = patient_id)
+
 
 # Se conservan únicamente las variables definidas para la entidad DIAGNOSTICO
 diagnostico <- diagnostico %>%
