1ebf3d14af1b7bfab3aa39b4b8d4595da815519d
Ines-gpm <inesgpmarco@gmail.com>
2026-07-31 19:31:57 +0200

    build_hijo y unificación con la nueva entidad EMBARAZO
    - Simplifica la construcción de la entidad HIJO
    - Sustituye la asignación de embarazos mediante proximidad de fechas por la relación directa con la entidad EMBARAZO
    - Elimina tablas auxiliares y filtrados redundantes
    - Añade controles de plausibilidad para peso y talla al nacimiento
    - Normaliza la creación de la fecha de nacimiento y mejora la documentación del script

--- R/04_build_hijo.R
+++ R/04_build_hijo.R
@@ -1,21 +1,23 @@
-# Importar las fuentes de datos necesarias para construir la entidad HIJO
-# hijo_neosoft, hijo_demograficos, madre_cartilla y embarazos_aux
-# se generan en 03_reconstruccion_embarazos.R
+# -----------------------------------------------------------------------------
+# ENTIDAD HIJO
+#
+# Objetivo:
+# Construir la entidad HIJO a partir de la información neonatal registrada en
+# NeoSoft y de los datos demográficos, vinculando cada recién nacido con el
+# embarazo previamente reconstruido.
+#
+# Se conservan únicamente los hijos pertenecientes a los embarazos incluidos en
+# la cohorte de estudio y se aplican controles básicos de calidad sobre las
+# variables clínicas
+# -----------------------------------------------------------------------------
 
-# Limpieza en 03_reconstruccion_embarazos.R
+# 1. Los objetos hijo_neosoft y hijo_demograficos se generan en 
+#    02_reconstruccion_embarazos.R y la entidad embarazo en
+#    03_build_embarazo.R
 
-# Creamos una tabla de madres válidas que emplearemos al final para filtrar
-madres_validas <- hijo_neosoft %>%
-  distinct(mother_patient_id) %>%
-  rename(id_madre = mother_patient_id) %>%
-  inner_join(
-    madre_cartilla %>%
-      distinct(patient_id) %>%
-      rename(id_madre = patient_id),
-    by = "id_madre"
-  )
+# 2. Limpieza en 02_reconstruccion_embarazos.R
 
-# Seleccionar los atributos principales que definen la entidad HIJO
+# 3. Seleccionar los atributos principales que definen la entidad HIJO
 hijo <- hijo_neosoft %>%
   select(
     patient_id,
@@ -28,102 +30,87 @@ hijo <- hijo_neosoft %>%
   ) %>%
   distinct()
 
-# Construir la fecha de nacimiento a partir del año y el mes disponibles.
-# Al desconocerse el día, se asigna el primer día del mes según la convención
-# definida en la documentación del proyecto
-hijo_demograficos <- hijo_demograficos %>%
+# 4. Aplicar un filtro de plausibilidad biológica sobre el peso y la talla al
+#    nacimiento
+hijo <- hijo %>%
   mutate(
-    fecha_nacimiento = as.Date(
-      sprintf("%04d-%02d-01", ano_nac, mes_nac)
-    )
+    
+    peso_nacimiento =
+      if_else(
+        peso_nacimiento < 500 |
+          peso_nacimiento > 5300,
+        
+        NA_real_,
+        peso_nacimiento
+      ),
+    
+    talla_nacimiento =
+      if_else(
+        talla_nacimiento < 21.4 |
+          talla_nacimiento > 65,
+        
+        NA_real_,
+        talla_nacimiento
+      )
   )
 
-# Conservar únicamente las variables necesarias para incorporar la fecha de nacimiento
-hijo_demograficos <- hijo_demograficos %>%
-  select(patient_id, fecha_nacimiento)
+# 5. Construir la fecha de nacimiento a partir del año y el mes disponibles
+
+# Al desconocerse el día, se asigna el día 15 del mes según la convención
+# definida en la documentación del proyecto
+fecha_nacimiento <- hijo_demograficos %>%
+  mutate(
+    fecha_nacimiento = make_date(
+      ano_nac,
+      mes_nac,
+      15
+    )
+  ) %>%
+  filter(
+    between(year(fecha_nacimiento), 2018, 2023)
+  ) %>%
+  select(
+    patient_id,
+    fecha_nacimiento
+  )
 
 # Incorporar la fecha de nacimiento a la entidad HIJO
 hijo <- hijo %>%
   left_join(
-    hijo_demograficos %>%
-      select(patient_id, fecha_nacimiento),
-    by = "patient_id"
+    fecha_nacimiento,
+    by="patient_id"
+  ) %>%
+  filter(!is.na(fecha_nacimiento))
+
+# 6. Adaptar los nombres de las variables a la nomenclatura definida en el modelo E/R
+hijo <- hijo %>% 
+  rename(
+    id_hijo = patient_id, 
+    edad_gestacional_nacimiento = edad_gestacional
   )
 
-# Adaptar los nombres de las variables a la nomenclatura definida en el modelo E/R
-hijo <- hijo %>% 
-  rename(id_hijo = patient_id, 
-         edad_gestacional_nacimiento = edad_gestacional)
-
-# Construir una tabla auxiliar con los hijos y la madre correspondiente
-hijos_aux <- hijo_neosoft %>%
-  select(
-    patient_id,
-    mother_patient_id
-  ) %>%
-  left_join(
-    hijo_demograficos,
-    by = "patient_id"
-  ) %>%
-  rename(
-    id_hijo = patient_id,
-    id_madre = mother_patient_id
-  ) %>%
-  distinct()
-
-hijos_aux <- hijos_aux %>%
-  left_join(
-    embarazos_aux %>%
-      select(
-        id_embarazo,
-        id_madre,
-        fecha_parto
-      ),
-    by = "id_madre"
-  ) %>%
-  mutate(diferencia = abs(as.numeric(fecha_nacimiento - fecha_parto))
-  ) %>%
-  group_by(id_hijo) %>%
-  slice_min(diferencia,
-            n = 1,
-            with_ties =  FALSE
-  ) %>%
-  ungroup()
-
+# 7. Asociar cada recién nacido con el embarazo previamente reconstruido
 hijo <- hijo %>%
   left_join(
-    hijos_aux %>%
+    embarazo %>%
       select(
         id_hijo,
         id_embarazo
       ),
-    by = "id_hijo")
+    by = "id_hijo"
+  )
 
-# Formatear las fechas siguiendo el estándar YYYYMMDD  
+# 8. Conservar únicamente los hijos cuyo embarazo pertenece al periodo de estudio
+hijo <- hijo %>%
+  filter(
+    !is.na(id_embarazo)
+  )
+
+# 9. Formatear las fechas y reordenar variables 
 hijo <- hijo %>%
   mutate(
     fecha_nacimiento = format(fecha_nacimiento, "%Y%m%d")
-  )
-
-# Filtramos solo las madres que sean válidas y por lo tanto los hijos válidos
-hijo <- hijo %>%
-  left_join(
-    hijo_neosoft %>%
-      select(patient_id, mother_patient_id) %>%
-      rename(
-        id_hijo = patient_id,
-        id_madre = mother_patient_id
-      ),
-    by = "id_hijo"
   ) %>%
-  semi_join(
-    madres_validas,
-    by = "id_madre"
-  ) %>%
-  select(-id_madre)
-
-# Reordenar variables 
-hijo <- hijo %>%
   select(
     id_hijo,
     id_embarazo,
@@ -134,6 +121,8 @@ hijo <- hijo %>%
     malformation_cd,
     muerte_neonatal,
     fecha_nacimiento
-  )
+  ) %>%
+  distinct() 
 
+# 10. Exportar la entidad HIJO
 write_csv(hijo,"Y:/PROYECTOS/2024 Salud perinatal (Luis-Aída-Sol)/Desarrollo/Datos_transformados/hijo.csv")
