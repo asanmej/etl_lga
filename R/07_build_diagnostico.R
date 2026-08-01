103d6dd301ca6d1fe61b3cfb8df031f84099163c
Ines-gpm <inesgpmarco@gmail.com>
2026-08-01 10:33:40 +0200

    Actualización del formato de exportar e importar y eliminar fuentes redundantes
    - Sustituir el filtrado mediante madres_validas por la entidad MADRE para mantener la coherenci
    - Filtrar los diagnósticos de hijos utilizando directamente la entidad HIJO
    - Eliminar dependencias innecesarias (hijo_neosoft y madre_cartilla)
    - Simplificar el flujo de limpieza y filtrado mediante semi_join()
    - Mantener el identificador original del paciente hasta la unificación y renombrarlo únicamente al final
    - Automatizar rutas mediante las variables de configuración
    - Reorganizar el script, numerar los pasos y mejorar la documentación y los comentarios

--- R/07_build_diagnostico.R
+++ R/07_build_diagnostico.R
@@ -1,66 +1,58 @@
-# Importar los archivos que contienen los diagnósticos registrados para madres e hijos
+# -----------------------------------------------------------------------------
+# ENTIDAD DIAGNOSTICO
+#
+# Objetivo:
+# Construir una única entidad de diagnósticos integrando la información
+# procedente de OMI y CMBD para las madres y de OMI para los recién nacidos
+#
+# Cada registro conserva el origen del diagnóstico y el tipo de paciente
+# (madre o hijo), permitiendo su trazabilidad
+# -----------------------------------------------------------------------------
+
+# 1. Importar los datos necesarios para construir la entidad:
+
 # Diagnósticos de la madre
-madre_diag_omi <- read_delim("Y:/PROYECTOS/2024 Salud perinatal (Luis-Aída-Sol)/Desarrollo/Datos/csv_20240508/madre_diag_omi.csv", 
+madre_diag_omi <- read_delim(FILE_MADRE_DIAG_OMI,
                              delim = "|", escape_double = FALSE, trim_ws = TRUE)
 
-madre_diag_cmbd <- read_delim("Y:/PROYECTOS/2024 Salud perinatal (Luis-Aída-Sol)/Desarrollo/Datos/csv_20240508/madre_diag_cmbd.csv", 
+madre_diag_cmbd <- read_delim(FILE_MADRE_DIAG_CMBD,
                               delim = "|", escape_double = FALSE, trim_ws = TRUE)
 
 # Diagnósticos del hijo
-hijo_diag_omi <- read_delim("Y:/PROYECTOS/2024 Salud perinatal (Luis-Aída-Sol)/Desarrollo/Datos/csv_20240508/hijo_diag_omi.csv", 
+hijo_diag_omi <- read_delim(FILE_HIJO_DIAG_OMI,
                             delim = "|", escape_double = FALSE, trim_ws = TRUE)
 
-# Archivo de hijos para filtrar a las madres
-hijo_neosoft <- read_delim("Y:/PROYECTOS/2024 Salud perinatal (Luis-Aída-Sol)/Desarrollo/Datos/csv_20240508/hijo_neosoft.csv", 
-                           delim = "|", escape_double = FALSE, trim_ws = TRUE)
+# 2. Limpieza y filtrado de los diagnósticos:
 
-# Creamos una tabla de madres válidas que emplearemos al final para filtrar
-madres_validas <- hijo_neosoft %>%
-  distinct(mother_patient_id) %>%
-  rename(id_madre = mother_patient_id) %>%
-  inner_join(
-    madre_cartilla %>%
-      distinct(patient_id) %>%
-      rename(id_madre = patient_id),
-    by = "id_madre"
-  )
-
-# Limpieza de los datos y filtramos haciendo uso de madres_validas para los diagnosticos
-# y la entidad HIJO para los diagnosticos de los hijos (ejecutada anterior mente en Main.R)
+# Se conservan únicamente las madres presentes en la entidad MADRE y los
+# recién nacidos presentes en la entidad HIJO, garantizando la coherencia
+# entre todas las entidades del modelo
 madre_diag_cmbd <- madre_diag_cmbd %>%
-  rename(diag_dt = fecing)
+  clean_names() %>%
+  distinct() %>%
+  rename(diag_dt = fecing) %>%
+  semi_join(
+    madre %>% select(id_madre),
+    by = c("patient_id"="id_madre")
+  )
 
 madre_diag_omi <- madre_diag_omi %>%
   clean_names() %>%
   distinct() %>%
-  rename(id_madre = patient_id) %>%
   semi_join(
-    madres_validas,
-    by = "id_madre"
-  ) %>%
-  rename(patient_id = id_madre)
-
-madre_diag_cmbd <- madre_diag_cmbd %>%
-  clean_names() %>%
-  distinct() %>%
-  rename(id_madre = patient_id) %>%
-  semi_join(
-    madres_validas,
-    by = "id_madre"
-  ) %>%
-  rename(patient_id = id_madre)
-
-hijo_diag_omi <- hijo_diag_omi %>%
-  clean_names() %>%
-  distinct()
-
-hijo_diag_omi <- hijo_diag_omi %>%
-  semi_join(
-    hijo %>% select(id_hijo),
-    by = c("patient_id" = "id_hijo")
+    madre %>% select(id_madre),
+    by = c("patient_id"="id_madre")
   )
 
-# Identificar el tipo de paciente y el origen de cada diagnóstico
+hijo_diag_omi <- hijo_diag_omi %>%
+  clean_names() %>%
+  distinct() %>%
+  semi_join(
+    hijo %>% select(id_hijo),
+    by = c("patient_id"="id_hijo")
+  )
+
+# 3. Identificar el tipo de paciente y el origen de cada diagnóstico
 diag_omi <- madre_diag_omi %>%
   mutate(
     tipo_paciente = "Madre",
@@ -79,29 +71,32 @@ diag_hijo <- hijo_diag_omi %>%
     origen = "OMI"
   )
 
-# Se unifican los diagnósticos procedentes de las distintas fuentes de información
-# en una única entidad, conservando el origen de cada registro.
+# 4. Unificar los diagnósticos procedentes de las distintas fuentes de información
+#    en una única entidad, conservando el origen de cada registro
 diagnostico <- bind_rows(
   diag_omi,
   diag_cmbd,
   diag_hijo
 )
 
-# Se crea el identificador único de diagnóstico
+# 5. Se crea el identificador único de diagnóstico
 diagnostico <- diagnostico %>%
   mutate(
     id_diagnostico = row_number()
   ) %>%
   relocate(id_diagnostico)
 
-# Renombrar el identificador del paciente para ajustarlo al modelo E/R:
+# 6. Renombrar el identificador del paciente para ajustarlo al modelo E/R:
+# En este punto las tres fuentes mantienen el identificador original
+# (patient_id), que pasa a denominarse id_paciente
+
 # El atributo id_paciente puede hacer referencia tanto a una madre como a un hijo,
-# diferenciándose mediante la variable tipo_paciente.
+# diferenciándose mediante la variable tipo_paciente
 diagnostico <- diagnostico %>%
   rename(id_paciente = patient_id)
 
 
-# Se conservan únicamente las variables definidas para la entidad DIAGNOSTICO
+# 7. Se conservan únicamente las variables definidas para la entidad DIAGNOSTICO
 diagnostico <- diagnostico %>%
   select(id_diagnostico,
          id_paciente,
@@ -112,10 +107,14 @@ diagnostico <- diagnostico %>%
          origen
   )
 
-# Formatear las fechas siguiendo el estándar YYYYMMDD
+# 8. Formatear las fechas siguiendo el estándar YYYYMMDD
 diagnostico <- diagnostico %>%
   mutate(
     diag_dt = format(diag_dt, "%Y%m%d")
   )
 
-write_csv(diagnostico,"Y:/PROYECTOS/2024 Salud perinatal (Luis-Aída-Sol)/Desarrollo/Datos_transformados/diagnostico.csv")
+# 9. Exportar la entidad DIAGNOSTICO 
+write_csv(
+  diagnostico,
+  file.path(PATH_TRANSFORMADOS,"diagnostico.csv")
+)
