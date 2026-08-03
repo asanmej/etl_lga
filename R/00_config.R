59673c81925560f83d5306061966cbef893c4752
Ines-gpm <inesgpmarco@gmail.com>
2026-08-03 17:21:23 +0200

    Cierre del script de configuración

--- R/00_config.R
+++ R/00_config.R
@@ -10,6 +10,8 @@ pkg <- c(
   "tidyverse",
   ## Limpieza de nombres de variables
   "janitor",
+  ## Para establecer el directorio del proyecto
+  "here",
   
   # 2. Análisis de calidad (QA)
   ## Resúmenes descriptivos y calidad de datos
@@ -46,11 +48,45 @@ lapply(pkg, function (x){if(!require(x, character.only = T)){install.packages(x 
 rm(pkg)
 
 
-## Rutas del proyecto empleadas en Main.R
-
-PATH_PROYECTO <- "Y:/PROYECTOS/2024 Salud perinatal (Luis-Aída-Sol)/Desarrollo"
+## Directorios
+PATH_PROYECTO <- here()
 
 PATH_DATOS <- file.path(PATH_PROYECTO, "Datos")
-PATH_R <- file.path(PATH_DATOS, "R")
+PATH_R <- file.path(PATH_PROYECTO, "R")
 PATH_RESULTS <- file.path(PATH_PROYECTO, "Results")
 
+## Datos transformados
+PATH_TRANSFORMADOS <- file.path(PATH_PROYECTO, "Datos_transformados")
+
+## Crear automáticamente las carpetas de salida si no existen
+dirs <- c(
+  PATH_RESULTS,
+  PATH_TRANSFORMADOS
+)
+
+invisible(
+  lapply(
+    dirs,
+    dir.create,
+    recursive = TRUE,
+    showWarnings = FALSE
+  )
+)
+
+## CSV originales
+PATH_CSV_20240508 <- file.path(PATH_DATOS, "csv_20240508")
+PATH_CSV_20240625 <- file.path(PATH_DATOS, "csv_20240625")
+PATH_CSV_20260616 <- file.path(PATH_DATOS, "csv20260616")
+
+
+## MADRE
+FILE_MADRE_CARTILLA      <- file.path(PATH_CSV_20240625, "madre_cartilla.csv")
+FILE_MADRE_DEMOGRAFICOS  <- file.path(PATH_CSV_20240508, "madre_demograficos.csv")
+FILE_MADRE_DGP           <- file.path(PATH_CSV_20260616, "madre_dgp.csv")
+FILE_MADRE_DIAG_OMI      <- file.path(PATH_CSV_20240508, "madre_diag_omi.csv")
+FILE_MADRE_DIAG_CMBD     <- file.path(PATH_CSV_20240508, "madre_diag_cmbd.csv")
+
+## HIJO
+FILE_HIJO_NEOSOFT        <- file.path(PATH_CSV_20240508, "hijo_neosoft.csv")
+FILE_HIJO_DEMOGRAFICOS   <- file.path(PATH_CSV_20240508, "hijo_demograficos.csv")
+FILE_HIJO_DIAG_OMI      <- file.path(PATH_CSV_20240508, "hijo_diag_omi.csv")
